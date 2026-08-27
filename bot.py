bdc305203daea1341daa3a3caa99f5227088c246
Deen <deen2546gg@gmail.com>
2026-08-28 01:44:31 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -112,17 +112,20 @@ YTDL_OPTIONS = {
     'geo_bypass': True,
     'extractor_args': {
         'youtube': {
-            'player_client': ['android', 'ios', 'mweb'],
+            'player_client': ['web_creator', 'android', 'ios'],
             'skip': ['hls', 'dash']
         }
     },
-    'headers': {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+    'http_headers': {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Language': 'en-us,en;q=0.5',
     }
 }
 
 if os.path.exists('cookies.txt'):
     YTDL_OPTIONS['cookiefile'] = 'cookies.txt'
+    print("ตรวจพบไฟล์ cookies.txt -> ใช้งานระบบ คุกกี้ เพื่อหลบการบล็อกเรียบร้อยแล้ว!")
 
 FFMPEG_OPTIONS = {
     'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
